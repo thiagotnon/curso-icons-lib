f4f5bd36c3cf7bdb55914fc813f1a0941474136e
Thiago Natan <36268854+thiagotnon@users.noreply.github.com>
2025-06-20 11:03:46 -0300

    aula_07

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,26 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: ['src/components'],
+      exclude: ['src/assets', 'src/index.ts'],
+      insertTypesEntry: true,
+      outDir: 'dist',
+    }),
+  ],
+  build: {
+    lib: {
+      entry: 'src/components/index.ts',
+      name: 'paprika-icons',
+      formats: ['es', 'cjs'],
+      fileName: (format) => `index.${format}.js`,
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+    },
+  },
 });
